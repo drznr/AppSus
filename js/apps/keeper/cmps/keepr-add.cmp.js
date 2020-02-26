48bed4ba19dce08d1539bb186124172fb99e7bf9
drznr <44776027+drznr@users.noreply.github.com>
2020-02-26 20:35:15 +0200

    day1final

--- js/apps/keeper/cmps/keepr-add.cmp.js
+++ js/apps/keeper/cmps/keepr-add.cmp.js
@@ -1,14 +1,76 @@
-
+import { utilService } from '../../../service/utils.js';
 
 export default {
     template: `
     <section class="keeper-add">
-        <input type="text" placeholder="Whats on your mind..." />
+    <div class="add-input">
+        <input type="text" 
+        :placeholder="compPlaceholder" 
+        v-model="userInp" 
+        @keyup.enter="addNote" 
+        class="info-inp" />
+        <input type="text" placeholder="Title" v-model="noteTitle" @keyup.enter="addNote" v-show="userInp" />
+        <span>
+            <input type="radio" name="note-type" id="txtNote" value="NoteText" v-model="noteType" />
+            <label for="txtNote">
+                <img src="../../../../imgs/icons/font.png" title="Add a text note" class="add-note-icon" />
+            </label>
+            <input type="radio" name="note-type" id="imgNote" value="NoteImg" v-model="noteType" />
+            <label for="imgNote">
+                <img src="../../../../imgs/icons/photo.png" title="Add an image note" class="add-note-icon" />
+            </label>
+            <input type="radio" name="note-type" id="todosNote" value="NoteTodos" v-model="noteType" />
+            <label for="todosNote">
+                <img src="../../../../imgs/icons/list.png" title="Add to do's note" class="add-note-icon" />
+            </label>
+            <input type="radio" name="note-type" id="videoNote" value="NoteVideo" v-model="noteType" />
+            <label for="videoNote">
+                <img src="../../../../imgs/icons/vid.png" title="Add a video note" class="add-note-icon" />
+            </label>
+            <input type="radio" name="note-type" id="audioNote" value="NoteAudio" v-model="noteType" />
+            <label for="audioNote">
+                <img src="../../../../imgs/icons/aud.png" title="Add an audio note" class="add-note-icon" />
+            </label>
+        </span>
+    </div>
     </section>
     `,
     data() {
         return {
-
+            noteType: 'NoteText',
+            inpPlaceholder: '',
+            userInp: '',
+            noteTitle: ''
+        }
+    },
+    computed: {
+        compPlaceholder() {
+            switch (this.noteType) {
+                case 'NoteText':
+                    this.inpPlaceholder = 'Whats on your mind...';
+                    break;
+                case 'NoteImg':
+                    this.inpPlaceholder = 'Enter Image URL...';
+                    break;
+                case 'NoteTodos':
+                    this.inpPlaceholder = 'Enter commas seperated to do\'s...';
+                    break;
+                case 'NoteVideo':
+                    this.inpPlaceholder = 'Enter Youtube URL...';
+                    break;
+                case 'NoteAudio':
+                    this.inpPlaceholder = 'Enter Audio URL...';
+                    break;
+                default:
+                    break;
+            }
+            return this.inpPlaceholder;
+        }
+    },
+    methods: {
+        addNote() {
+            this.userInp = (this.noteType === 'NoteVideo') ? utilService.getYoutubeVidId(this.userInp) : this.userInp;
+            this.$emit('note-add', {input: this.userInp, type: this.noteType, title: this.noteTitle})
         }
     }
 }
--- js/apps/keeper/keeper-app.cmp.js
+++ js/apps/keeper/keeper-app.cmp.js
@@ -1,13 +1,32 @@
+import { keeperService } from './services/keeper-service.js';
 import keeprAddCmp from './cmps/keepr-add.cmp.js';
 import keeperListCmp from './cmps/keeper-list.cmp.js';
 
 export default {
     template: `
         <section class="keeper">
-            <keeper-add></keeper-add>
-            <keeper-list></keeper-list>
+            <keeper-add @note-add="addNote"></keeper-add>
+            <keeper-list :notes="notes"></keeper-list>
         </section>
     `,
+    data() {
+        return {
+            notes: null
+        }
+    },
+    created() {
+            keeperService.getNotes().then(notes=> {
+                this.notes = JSON.parse(JSON.stringify(notes));
+            })
+            .catch(() => {
+              debugger
+            });
+    },
+    methods: {
+        addNote(noteData) {
+            debugger
+        }
+    },
     components: {
         'keeper-add': keeprAddCmp,
         'keeper-list': keeperListCmp
