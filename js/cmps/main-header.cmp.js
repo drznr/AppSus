9845601e5edc2c133cb57e33cee26ef5d77a48cf
drznr <44776027+drznr@users.noreply.github.com>
2020-02-26 14:23:38 +0200

    header done

--- js/cmps/main-header.cmp.js
+++ js/cmps/main-header.cmp.js
@@ -2,12 +2,16 @@ export default {
     template: `
         <header class="main-header">
             <img src="../../imgs/logo-white.png" alt="logo"/>
+            <input type="checkbox" id="mainBurger"/>
+            <label for="mainBurger" class="main-hamburger">
+            <span>|</span>
+            </label>
             <ul class="main-nav">
-                <li><router-link to="/">Home</router-link></li>
-                <li><router-link to="/books">Miss Books</router-link></li>
-                <li><router-link to="/keeper">Mister Keeper</router-link></li>
-                <li><router-link to="/emails">Miss Emails</router-link></li>
-                <li><router-link to="/about">About</router-link></li>
+                <li><router-link to="/" exact>Home</router-link></li>
+                <li><router-link to="/books" exact>Miss Books</router-link></li>
+                <li><router-link to="/keeper" exact>Mister Keeper</router-link></li>
+                <li><router-link to="/emails" exact>Miss Emails</router-link></li>
+                <li><router-link to="/about" exact>About</router-link></li>
             </ul>
         </header>
     `
