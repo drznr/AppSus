927f31f1064bcd85636f78d0adf165962a97a693
drznr <44776027+drznr@users.noreply.github.com>
2020-03-02 14:54:51 +0200

    note view added

--- js/apps/keeper/cmps/note-preview.cmp.js
+++ js/apps/keeper/cmps/note-preview.cmp.js
@@ -2,42 +2,47 @@ import { dynamicNotes } from './dynamic-cmps.js';
 
 export default {
     template: `
-        <div class="note-card" :style="{backgroundColor: note.styles.backgroundColor}">
-        <img v-if="note.isPinned" src="imgs/icons/color-pin.png" class="pin" />
-        <component :is="note.type"
+    <span>
+        <div class="cover" :class="{viewed: onView}" @click="onView = !onView"></div>
+            <div class="note-card" :style="{backgroundColor: note.styles.backgroundColor}" :class="{viewed: onView}">
+            <img v-if="note.isPinned" src="imgs/icons/color-pin.png" class="pin" />
+            <component :is="note.type"
                    :note="note"
                     @todo-toggle="passTodos"
                     @todo-added="passTodo"
                     @remove-todo="passTodoIdx"
-        ></component>
+            ></component>
 
-        <input 
-            type="text" 
-            v-if="onEdit" 
-            v-model="newTxt" 
-            @keyup.enter="$emit('txt-change', {txt:newTxt, id: note.id, type: note.type});onEdit=false;newTxt=''"
-            :placeholder="'Enter ' + noteTypePlaceholder"
-            />
+            <input 
+                type="text" 
+                v-if="onEdit" 
+                v-model="newTxt" 
+                @keyup.enter="$emit('txt-change', {txt:newTxt, id: note.id, type: note.type});onEdit=false;newTxt=''"
+                :placeholder="'Enter ' + noteTypePlaceholder"
+                />
             <div class="controls">
-            <button class="card-btn" @click="$emit('pinned', note.id, note.isPinned)"><img src="imgs/icons/pin.png" title="Pin note" /></button>
-            <button class="card-btn">
-            <label>
-                <input type="color" @change="$emit('colored', note.id, color)" v-model="color" />
-                <img src="imgs/icons/color.png" title="Change color" />
-            </label>
-            </button>
-            <button v-if="note.type!=='NoteTodos'" class="card-btn" @click="toggleEdit"><img src="imgs/icons/edit.png" title="Edit note"/></button>
-            <router-link v-if="note.type === 'NoteText'" class="card-btn" :to="'emails/compose/' + note.id + '/' + title + '/' + txt"><img src="imgs/icons/send.png" title="Send note"/></router-link>
-            <button class="card-btn" @click="$emit('removed', note.id)"><img src="imgs/icons/bin.png" title="Remove note" /></button>
+                <button class="card-btn" @click="$emit('pinned', note.id, note.isPinned)"><img src="imgs/icons/pin.png" title="Pin note" /></button>
+                <button class="card-btn">
+                <label>
+                    <input type="color" @change="$emit('colored', note.id, color)" v-model="color" />
+                    <img src="imgs/icons/color.png" title="Change color" />
+                </label>
+                </button>
+                <button class="card-btn" @click="onView = !onView"><img :src="onView ? 'imgs/icons/hide.png' : 'imgs/icons/eye.png'" :title="onView ? 'Hide note' : 'View note'" /></button>
+                <button v-if="note.type!=='NoteTodos'" class="card-btn" @click="toggleEdit"><img src="imgs/icons/edit.png" title="Edit note"/></button>
+                <router-link v-if="note.type === 'NoteText'" class="card-btn" :to="'emails/compose/' + note.id + '/' + title + '/' + txt"><img src="imgs/icons/send.png" title="Send note"/></router-link>
+                <button class="card-btn" @click="$emit('removed', note.id)"><img src="imgs/icons/bin.png" title="Remove note" /></button>
             </div>
         </div>
+    </span>
     `,
     props: ['note'],
     data() {
         return {
             color: this.note.styles.backgroundColor,
             newTxt: '',
-            onEdit: false
+            onEdit: false,
+            onView: false
         }
     },
     methods: {
